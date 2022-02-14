84b244651224917c40fe58320e198661041b11fe
thonker-r7 <tim@private.local>
2022-02-14 12:55:11 -0500

    migration from private repo

--- reddit/setup-Reddit-User-Media-Downloader.sh
+++ reddit/setup-Reddit-User-Media-Downloader.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Tim H 2021
+# Tim H 2021, 2022
 # Installs Reddit-User-Media-Downloader on the local system for archiving
 # an individual user's Reddit posts/comments. It seems to only download
 # images and videos, not text posts?
@@ -23,7 +23,7 @@ python3 -m pip install --upgrade pip
 
 git clone --recursive https://github.com/MonkeyMaster64/Reddit-User-Media-Downloader-Public.git
 python3 -m venv ~/envs/Reddit-User-Media-Downloader-Public
-source "~/envs/Reddit-User-Media-Downloader-Public/bin/activate"
+source "$HOME/envs/Reddit-User-Media-Downloader-Public/bin/activate"
 cd ./Reddit-User-Media-Downloader-Public/
 
 # Following their instructions for Linux install causes issues, so I've
@@ -31,14 +31,15 @@ cd ./Reddit-User-Media-Downloader-Public/
 # Prob easier to just use the Docker container but not everyone knows how
 # to do that.
 
+# opencv-python breaks it in the requirements file
 # pip3 install -r requirements.txt
 pip3 install imagededup
 pip3 install youtube_dl
 pip3 install cython
 pip3 install requests
 
-# the problematic one
-#pip3 install opencv-python==4.5.2
+# the problematic one, so just dropping the version requirement
+# pip3 install opencv-python==4.5.2
 pip3 install opencv-python
 
 # example usage - download last 2 posts by reddit admin sodypop
